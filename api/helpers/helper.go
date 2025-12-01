c338a5c9a0dec0f4cd4f0f53c0a3b7674c23ffc8
imshubham024 <shubham24042003@gmail.com>
2025-12-01 10:56:55 +0530

    helper functionality

--- api/helpers/helper.go
+++ api/helpers/helper.go
@@ -12,6 +12,12 @@ func EnforceHTTP(url string) string{
 	return url
 }
 
-func RemoveDomainError(url string) string{
-	
+func RemoveDomainError(url string) bool{
+	if url==os.Getenv("DOMAIN") {
+		return false
+	}
+	newURL:=strings.Replace(url,"https://","",1)
+	newURL=strings.Replace(newURL,"http://","",1)
+	newURL=strings.Split(newURL,"/")[0]
+	return newURL!=os.Getenv("DOMAIN")
 }
